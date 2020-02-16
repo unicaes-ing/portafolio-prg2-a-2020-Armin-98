11a1f5d3fac968f8cd3cc40d1b5ccc5689eb8e0c
Armin-98 <emerson.rosales@catolica.edu.sv>
2020-02-16 13:01:03 -0600

    2019-RL-601

---
+++ Trabajos de la guia 1/Proyectos sin compilar/Libreria Universitaria/Libreria Universitaria/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Libreria_Universitaria
+{
+    partial class Libreria_Universitaria
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cob_books = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_price = new System.Windows.Forms.TextBox();
+            this.txt_count = new System.Windows.Forms.TextBox();
+            this.txt_subtotal = new System.Windows.Forms.TextBox();
+            this.txt_tax = new System.Windows.Forms.TextBox();
+            this.txt_pay = new System.Windows.Forms.TextBox();
+            this.btn_calc = new System.Windows.Forms.Button();
+            this.btn_clean = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Titulos de libros:";
+            // 
+            // cob_books
+            // 
+            this.cob_books.FormattingEnabled = true;
+            this.cob_books.Location = new System.Drawing.Point(119, 31);
+            this.cob_books.Name = "cob_books";
+            this.cob_books.Size = new System.Drawing.Size(270, 21);
+            this.cob_books.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Precio:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Cantidad:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Subtotal:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 206);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Impuesto (13%):";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(30, 241);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Total a pagar:";
+            // 
+            // txt_price
+            // 
+            this.txt_price.Location = new System.Drawing.Point(119, 69);
+            this.txt_price.Name = "txt_price";
+            this.txt_price.ReadOnly = true;
+            this.txt_price.Size = new System.Drawing.Size(100, 20);
+            this.txt_price.TabIndex = 7;
+            // 
+            // txt_count
+            // 
+            this.txt_count.Location = new System.Drawing.Point(119, 107);
+            this.txt_count.Name = "txt_count";
+            this.txt_count.Size = new System.Drawing.Size(100, 20);
+            this.txt_count.TabIndex = 8;
+            // 
+            // txt_subtotal
+            // 
+            this.txt_subtotal.Location = new System.Drawing.Point(119, 168);
+            this.txt_subtotal.Name = "txt_subtotal";
+            this.txt_subtotal.ReadOnly = true;
+            this.txt_subtotal.Size = new System.Drawing.Size(100, 20);
+            this.txt_subtotal.TabIndex = 9;
+            // 
+            // txt_tax
+            // 
+            this.txt_tax.Location = new System.Drawing.Point(119, 203);
+            this.txt_tax.Name = "txt_tax";
+            this.txt_tax.ReadOnly = true;
+            this.txt_tax.Size = new System.Drawing.Size(100, 20);
+            this.txt_tax.TabIndex = 10;
+            // 
+            // txt_pay
+            // 
+            this.txt_pay.Location = new System.Drawing.Point(119, 238);
+            this.txt_pay.Name = "txt_pay";
+            this.txt_pay.ReadOnly = true;
+            this.txt_pay.Size = new System.Drawing.Size(100, 20);
+            this.txt_pay.TabIndex = 11;
+            // 
+            // btn_calc
+            // 
+            this.btn_calc.Location = new System.Drawing.Point(33, 284);
+            this.btn_calc.Name = "btn_calc";
+            this.btn_calc.Size = new System.Drawing.Size(75, 23);
+            this.btn_calc.TabIndex = 12;
+            this.btn_calc.Text = "Calcular";
+            this.btn_calc.UseVisualStyleBackColor = true;
+            this.btn_calc.Click += new System.EventHandler(this.btn_calc_Click);
+            // 
+            // btn_clean
+            // 
+            this.btn_clean.Location = new System.Drawing.Point(144, 284);
+            this.btn_clean.Name = "btn_clean";
+            this.btn_clean.Size = new System.Drawing.Size(75, 23);
+            this.btn_clean.TabIndex = 13;
+            this.btn_clean.Text = "Limpiar";
+            this.btn_clean.UseVisualStyleBackColor = true;
+            this.btn_clean.Click += new System.EventHandler(this.btn_clean_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(248, 284);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(75, 23);
+            this.btn_exit.TabIndex = 14;
+            this.btn_exit.Text = "Salir";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // Libreria_Universitaria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 322);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_clean);
+            this.Controls.Add(this.btn_calc);
+            this.Controls.Add(this.txt_pay);
+            this.Controls.Add(this.txt_tax);
+            this.Controls.Add(this.txt_subtotal);
+            this.Controls.Add(this.txt_count);
+            this.Controls.Add(this.txt_price);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cob_books);
+            this.Controls.Add(this.label1);
+            this.Name = "Libreria_Universitaria";
+            this.Text = "Libreria Universitaria";
+            this.Load += new System.EventHandler(this.Libreria_Universitaria_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cob_books;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_price;
+        private System.Windows.Forms.TextBox txt_count;
+        private System.Windows.Forms.TextBox txt_subtotal;
+        private System.Windows.Forms.TextBox txt_tax;
+        private System.Windows.Forms.TextBox txt_pay;
+        private System.Windows.Forms.Button btn_calc;
+        private System.Windows.Forms.Button btn_clean;
+        private System.Windows.Forms.Button btn_exit;
+    }
+}
+
---
+++ Trabajos de la guia 1/Proyectos sin compilar/Libreria Universitaria/Libreria Universitaria/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Libreria_Universitaria
+{
+    public partial class Libreria_Universitaria : Form
+    {
+        public Libreria_Universitaria()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+        }
+
+        private void Libreria_Universitaria_Load(object sender, EventArgs e)
+        {
+            cob_books.Items.Add("Antropologia una guia para la existencia (Juan Manuel Burgos)");
+            cob_books.Items.Add("Filosofia para jovenes");
+            cob_books.Items.Add("Analisis de estados financieros");
+            cob_books.Items.Add("Programacion orientada a objetos");
+            cob_books.Items.Add("Curso de programacion en visual basic y C# vol 1");
+            cob_books.Items.Add("Curso de programacion en visual basic y C# vol 2");
+            cob_books.Items.Add("Curso de programacion en visual basic y C# vol 3");
+            cob_books.Items.Add("Curso de programacion en visual basic y C# vol 4");
+            cob_books.Items.Add("Curso de programacion en visual basic y C# vol 5");
+            cob_books.Items.Add("Uso y comprencion del uso de bases de datos");
+            cob_books.Items.Add("Python al descubierto");
+        }
+
+        private void btn_calc_Click(object sender, EventArgs e)
+        {
+            string books = Convert.ToString(cob_books.Text);
+            double price = 0, sub = 0, tax = 0, total = 0;
+            double cant = Convert.ToDouble(txt_count.Text);
+            if (cob_books.Text == "Antropologia una guia para la existencia (Juan Manuel Burgos)")
+            {
+                price = 20.00;
+                sub = price * cant;
+                tax = sub * 0.13;
+                total = sub + tax;
+                txt_price.Text = string.Format("{0:N2}", price);
+                txt_subtotal.Text = string.Format("{0:N2}", sub);
+                txt_tax.Text = string.Format("{0:N2}", tax);
+                txt_pay.Text = string.Format("{0:N2}", total);
+            }
+            else if (cob_books.Text == "Filosofia para jovenes")
+            {
+                price = 15.00;
+                price = price * cant;
+                tax = sub * 0.13;
+                total = sub + tax;
+                txt_price.Text = string.Format("{0:N2}", price);
+                txt_subtotal.Text = string.Format("{0:N2}", sub);
+                txt_tax.Text = string.Format("{0:N2}", tax);
+                txt_pay.Text = string.Format("{0:N2}", total);
+            }
+            else if (cob_books.Text == "Analisis de estados financieros")
+            {
+                price = 25.00;
+                sub = price * cant;
+                tax = sub * 0.13;
+                total = sub + tax;
+                txt_price.Text = string.Format("{0:N2}", price);
+                txt_subtotal.Text = string.Format("{0:N2}", sub);
+                txt_tax.Text = string.Format("{0:N2}", tax);
+                txt_pay.Text = string.Format("{0:N2}", total);
+            }
+            else if (cob_books.Text == "Programacion orientada a objetos")
+            {
+                price = 18.00;
+                sub = price * cant;
+                tax = sub * 0.13;
+                total = sub + tax;
+                txt_price.Text = string.Format("{0:N2}", price);
+                txt_subtotal.Text = string.Format("{0:N2}", sub);
+                txt_tax.Text = string.Format("{0:N2}", tax);
+                txt_pay.Text = string.Format("{0:N2}", total);
+            }
+            else if (cob_books.Text == "Curso de programacion en visual basic y C# vol 1")
+            {
+                price = 34.00;
+                sub = price * cant;
+                tax = sub * 0.13;
+                total = sub + tax;
+                txt_price.Text = string.Format("{0:N2}", price);
+                txt_subtotal.Text = string.Format("{0:N2}", sub);
+                txt_tax.Text = string.Format("{0:N2}", tax);
+                txt_pay.Text = string.Format("{0:N2}", total);
+            }
+            else if (cob_books.Text == "Curso de programacion en visual basic y C# vol 2")
+            {
+                price = 45.00;
+                sub = price * cant;
+                tax = sub * 0.13;
+                total = sub + tax;
+                txt_price.Text = string.Format("{0:N2}", price);
+                txt_subtotal.Text = string.Format("{0:N2}", sub);
+                txt_tax.Text = string.Format("{0:N2}", tax);
+                txt_pay.Text = string.Format("{0:N2}", total);
+            }
+            else if (cob_books.Text == "Curso de programacion en visual basic y C# vol 3")
+            {
+                price = 60.00;
+                sub = price * cant;
+                tax = sub * 0.13;
+                total = sub + tax;
+                txt_price.Text = string.Format("{0:N2}", price);
+                txt_subtotal.Text = string.Format("{0:N2}", sub);
+                txt_tax.Text = string.Format("{0:N2}", tax);
+                txt_pay.Text = string.Format("{0:N2}", total);
+            }
+            else if (cob_books.Text == "Curso de programacion en visual basic y C# vol 4")
+            {
+                price = 64.00;
+                sub = price * cant;
+                tax = sub * 0.13;
+                total = sub + tax;
+                txt_price.Text = string.Format("{0:N2}", price);
+                txt_subtotal.Text = string.Format("{0:N2}", sub);
+                txt_tax.Text = string.Format("{0:N2}", tax);
+                txt_pay.Text = string.Format("{0:N2}", total);
+            }
+            else if (cob_books.Text == "Curso de programacion en visual basic y C# vol 5")
+            {
+                price = 30.00;
+                sub = price * cant;
+                tax = sub * 0.13;
+                total = sub + tax;
+                txt_price.Text = string.Format("{0:N2}", price);
+                txt_subtotal.Text = string.Format("{0:N2}", sub);
+                txt_tax.Text = string.Format("{0:N2}", tax);
+                txt_pay.Text = string.Format("{0:N2}", total);
+            }
+            else if (cob_books.Text == "Uso y comprencion del uso de bases de datos")
+            {
+                price = 90.00;
+                sub = price * cant;
+                tax = sub * 0.13;
+                total = sub + tax;
+                txt_price.Text = string.Format("{0:N2}", price);
+                txt_subtotal.Text = string.Format("{0:N2}", sub);
+                txt_tax.Text = string.Format("{0:N2}", tax);
+                txt_pay.Text = string.Format("{0:N2}", total);
+            }
+            else if (cob_books.Text == "Python al descubierto")
+            {
+                price = 50.00;
+                sub = price * cant;
+                tax = sub * 0.13;
+                total = sub + tax;
+                txt_price.Text = string.Format("{0:N2}", price);
+                txt_subtotal.Text = string.Format("{0:N2}", sub);
+                txt_tax.Text = string.Format("{0:N2}", tax);
+                txt_pay.Text = string.Format("{0:N2}", total);
+            }
+            /*else if(txt_count.Text==null)
+            {
+                MessageBox.Show("Porfavor ingrese valores que se le piden ;)");
+            }*/
+
+        }
+
+        private void btn_clean_Click(object sender, EventArgs e)
+        {
+            txt_price.Clear();
+            cob_books.ResetText();
+            txt_subtotal.Clear();
+            txt_tax.Clear();
+            txt_pay.Clear();
+            txt_count.Clear();
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
